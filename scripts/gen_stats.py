daa65aaa1e9fc3ba1965a0e9ea89736054aabbcf
吳承翰 <79675972+Freedom-Hank@users.noreply.github.com>
2025-10-01 02:51:30 +0800

    Update gen_stats.py

--- scripts/gen_stats.py
+++ scripts/gen_stats.py
@@ -1,14 +1,13 @@
 import os
 import requests
 
+output_dir = "images"
+os.makedirs(output_dir, exist_ok=True)
+
 token = os.getenv("GITHUB_TOKEN")
 username = "Freedom-Hank"
 
-themes = {
-    "dark": "tokyonight",
-    "light": "github_light"
-}
-
+themes = {"dark": "tokyonight", "light": "github_light"}
 urls = {
     "stats": "https://github-readme-stats.vercel.app/api?username={u}&show_icons=true&count_private=true&theme={t}",
     "langs": "https://github-readme-stats.vercel.app/api/top-langs/?username={u}&layout=compact&count_private=true&theme={t}&card_width=500",
@@ -22,7 +21,7 @@ for mode, theme in themes.items():
         api_url = url.format(u=username, t=theme)
         res = requests.get(api_url, headers=headers)
         if res.status_code == 200:
-            filename = f"{name}-{mode}.svg"  # e.g. stats-dark.svg
+            filename = f"{output_dir}/{name}-{mode}.svg"
             with open(filename, "wb") as f:
                 f.write(res.content)
             print(f"✅ {filename} updated.")
